62c632602f288ef14869e714668d68f330ddafa4
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2023-01-02 19:08:06 +0700

    up

--- contracts/Edapp.sol
+++ contracts/Edapp.sol
@@ -2,7 +2,5 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Edapp {
-    uint public so_nguyen_duong = 1024012;
-    int public so_nguyen = -1291;
-    address private minteraddress;
+    receive() external payable{}
 }
