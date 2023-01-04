9f50256261ee24be99e71a18a02b3278eccabb14
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2023-01-04 14:09:11 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
+
+  
+
   return (
-    <div className="App">
-      Hello  
+    <div className="app">
+      <div className="balance">
+        Current Balance: <b>10ETH</b>
+      </div>
+      <button className="pay">Pay</button>
+      <button className="withdraw">Withdraw</button>
     </div>
   );
 }
