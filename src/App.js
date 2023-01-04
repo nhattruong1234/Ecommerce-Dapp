b6c1012807c3b35e189eda25d8d712c6199a8c54
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2023-01-04 17:33:17 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import Web3 from "web3";
+import { useState, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider"
 function App() {
 
-  
+  const [Web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  useEffect( ()=> {
+    const loadProvider = async ()=> {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        console.log('Ethereum successfully detected!')
+        provider.request({method: "eth_requestAccounts"})
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
+      } else {
+        console.error("please, Install Metamask")
+      }
+    }
+    loadProvider()
+  }, []);
 
   return (
     <div className="app">
