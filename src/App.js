c4cac2756fa3560130e4dc47002ce038dedab397
nhattruong1234 <nguyenphamnhattruong2001@gmail.com>
2023-01-05 13:57:06 +0700

    up

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ function App() {
     provider: null,
     web3: null,
   });
+  const [account, setAccount] = useState();
 
+  useEffect( ()=> {
+    const getAccount = async ()=> {
+      const accounts = await Web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+    Web3Api.web3 && getAccount()
+  },[Web3Api.web3]);
   useEffect( ()=> {
     const loadProvider = async ()=> {
       const provider = await detectEthereumProvider();
       if (provider) {
         console.log('Ethereum successfully detected!')
-        provider.request({method: "eth_requestAccounts"})
         setWeb3Api({
           web3: new Web3(provider),
           provider
         })
-      } else {
-        console.error("please, Install Metamask")
       }
     }
     loadProvider()
@@ -32,6 +37,17 @@ function App() {
       </div>
       <button className="pay">Pay</button>
       <button className="withdraw">Withdraw</button>
+      <button className="connectwallet" onDoubleClick={()=>
+          Web3Api.provider.request({method: "eth_requestAccounts"})
+        }>Connect wallet</button>
+      <p>
+        <b>
+          Accounts Address:
+        </b>
+      {
+        account ? " " + account : " Account not connected"
+      }
+      </p>
     </div>
   );
 }
